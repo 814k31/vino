a580e966928dc4a0ad59a97f889368d56befaa28
Blake Lovelace <blovelace@elementengineering.com.au>
2020-11-07 11:03:53 +0800

    Add very basic Batches controller class

---
+++ vino/Batches.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.ObjectModel;
+
+namespace vino
+{
+    public class Batches
+    {
+        private ObservableCollection<int> batches;
+
+        public Batches()
+        {
+            this.batches = new ObservableCollection<int>();
+        }
+
+        public void add(int newBatch)
+        {
+            this.batches.Add(newBatch);
+        }
+
+        public ReadOnlyObservableCollection<int> getCollection()
+        {
+            ReadOnlyObservableCollection<int> observableBatches = (
+                new ReadOnlyObservableCollection<int>(batches)
+            );
+
+            return observableBatches;
+        }
+    }
+}
