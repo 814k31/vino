335657f4f271f4caa87bba85048d63a5ac6ff3f0
Blake Lovelace <blovelace@elementengineering.com.au>
2020-11-07 16:07:19 +0800

    Allow Batch to update the view when the name is changed

--- vino/Batch.cs
+++ vino/Batch.cs
@@ -1,22 +1,36 @@
 ﻿using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace vino
 {
-    public class Batch
+    public class Batch : INotifyPropertyChanged
     {
         private string name;
         private DateTime dateCreated;
         private DateTime? dateUpdated;
 
-        public string Name
-        {
-            get { return this.name; }
-        }
+        public event PropertyChangedEventHandler PropertyChanged;
 
         public Batch(string name)
         {
             this.name = name;
             this.dateCreated = new DateTime();
             this.dateUpdated = null;
+        }
+
+        public string Name
+        {
+            get { return this.name; }
+            set {
+                name = value;
+                this.OnPropertyChanged();
+            }
+        }
+
+        void OnPropertyChanged([CallerMemberName] string name = "")
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
         }
     }
 }
