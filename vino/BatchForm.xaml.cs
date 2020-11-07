9e09c52ccfcf9955c1934614fed1267b4639d65b
Blake Lovelace <blovelace@elementengineering.com.au>
2020-11-07 16:10:43 +0800

    Add a simple form with an entry to edit the Batch's name

---
+++ vino/BatchForm.xaml.cs
@@ -0,0 +1,27 @@
+﻿using Xamarin.Forms;
+
+namespace vino
+{
+    public partial class BatchForm : ContentPage
+    {
+        private Batch batch;
+
+        public BatchForm(Batch batch)
+        {
+            this.batch = batch;
+            InitializeComponent();
+
+            BindingContext = this.batch;
+        }
+
+        async void onButtonClickedCreate(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+
+        async void onButtonClickedCancel(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PopModalAsync();
+        }
+    }
+}
