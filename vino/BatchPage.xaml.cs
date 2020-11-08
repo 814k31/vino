0c31ff2d025c89d4f3d699bf3afdc1dafbd24196
Blake Lovelace <blovelace@elementengineering.com.au>
2020-11-08 09:46:07 +0800

    Add missing BatchPage class

---
+++ vino/BatchPage.xaml.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace vino
+{
+    public partial class BatchPage : ContentPage
+    {
+        private Batches batches;
+        private Batch batch;
+
+        public Batch Batch
+        { get { return this.batch; } }
+
+        public BatchPage(Batches batches, Batch batch)
+        {
+            this.batches = batches;
+            this.batch = batch;
+
+            InitializeComponent();
+
+            BindingContext = this;
+        }
+    }
+}
