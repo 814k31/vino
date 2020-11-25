bcda63c8b34c7b58d277ca02bfca7a4f1674c88d
Blake Lovelace <blovelace@elementengineering.com.au>
2020-11-25 21:06:24 +0800

    Add put and post method to RestService

--- vino/models/RestService.cs
+++ vino/models/RestService.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Diagnostics;
 using System.Net.Http;
+using System.Text;
 using System.Threading.Tasks;
 
 namespace vino.models
@@ -30,6 +31,46 @@ namespace vino.models
             }
 
             return json;
+        }
+
+        public async Task<String> post(String url, String json)
+        {
+            Uri uri = new Uri(url);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            String jsonResponse = null;
+
+            try
+            {
+                HttpResponseMessage response = await this.client.PostAsync(uri, content);
+                jsonResponse = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine(@"\t Post result!!! {0}", jsonResponse);
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine(@"\tERROR {0}", error.Message);
+            }
+
+            return jsonResponse;
+        }
+
+        public async Task<String> put(String url, String json)
+        {
+            Uri uri = new Uri(url);
+            StringContent content = new StringContent(json, Encoding.UTF8, "application/json");
+            String jsonResponse = null;
+
+            try
+            {
+                HttpResponseMessage response = await this.client.PutAsync(uri, content);
+                jsonResponse = await response.Content.ReadAsStringAsync();
+                Debug.WriteLine(@"\t Put result!!! {0}", jsonResponse);
+            }
+            catch (Exception error)
+            {
+                Debug.WriteLine(@"\tERROR {0}", error.Message);
+            }
+
+            return jsonResponse;
         }
     }
 }
